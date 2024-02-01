ae65d113ab8cd65f491e8f958833ce7ea5b5fffb
Geofrey Onyango <geofreyonyango766@gmail.com>
2024-02-01 13:48:17 +0300

    ss

--- Main-Page/TopNotification.js
+++ Main-Page/TopNotification.js
@@ -29,61 +29,11 @@ function displayNotification() {
           notificationIndex++;
           displayNotification(); // Display the next notification after the interval
         }, 1000); // Exit animation duration
-      }, 1000); // Entrance duration
+      }, 9000); // Entrance duration
     },8000); //  Display animation duration
   }
 }
 
 // Start the sequence by displaying the first notification after an interval
-setTimeout(displayNotification, 5000); // Initial delay before displaying the first notification
+setTimeout(displayNotification, 50000); // Initial delay before displaying the first notification
 
-
-// Function to update the countdown
-function updateCountdown() {
-const targetDate = new Date("2023-10-26");
-const currentDate = new Date();
-const timeDifference = targetDate - currentDate;
-
-const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-document.getElementById("days").textContent = formatTime(days);
-document.getElementById("hours").textContent = formatTime(hours);
-document.getElementById("minutes").textContent = formatTime(minutes);
-document.getElementById("seconds").textContent = formatTime(seconds);
-}
-
-// Function to format time (e.g., add leading zeros)
-function formatTime(time) {
-return time < 10 ? `0${time}` : time;
-}
-
-// Initial update
-updateCountdown();
-
-// Update the countdown every second
-setInterval(updateCountdown, 1000);
-
-
-
-
-
-
-
-//Countdown js starts
-   // Set the total number of items and the completed items
-   const totalItems = 22;
-   const completedItems = 22;
-
-   // Calculate the percentage
-   const percentage = (completedItems / totalItems) * 100;
-
-   // Update the progress bar and label
-   const progressFill = document.getElementById('progress-fill');
-   const progressLabel = document.getElementById('progress-label');
-
-   progressFill.style.width = percentage + '%';
-   progressLabel.innerText = percentage.toFixed(2) +  '%';
-   //Countdown js Ends
